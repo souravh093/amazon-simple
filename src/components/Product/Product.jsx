23581973f5d3c35c498dc0ac796f1d4957fbd77e
Sourav halder <souravh093@gmail.com>
2023-03-27 15:47:54 +0600

    product components done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import noImage from '../../images/no-image.jpg';
 
 const Product = ({data}) => {
     return (
         <div>
             <div className="card w-full bg-base-100 border border-gray-300">
-            <figure><img src={data.img} alt="Shoes" /></figure>
+            <figure><img src={data.img ? data.img : noImage} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{data.name}</h2>
-                <p>Price: ${data.price}</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Add Cart</button>
-                </div>
+                <p className='text-xl font-bold'>Price: ${data.price}</p>
+                <p>Manufacturer: {data.seller}</p>
+                <p>Rating: {data.ratings} star</p>
             </div>
+                <button className="py-3 rounded-b-2xl  bg-[#FFE0B3] text-gray-700 hover:bg-yellow-100 transition">Add Cart</button>
             </div>
         </div>
     );
