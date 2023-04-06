2778d996b767ba391f33565baa71fa529dc0cd65
Sourav halder <souravh093@gmail.com>
2023-04-06 15:36:02 +0600

    cart add in order page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import Review from "../Review/Review";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
   return (
     <div className="grid grid-cols-5">
       {/* product container */}
-      <div className="col-span-4 grid grid-cols-3 p-10 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-        <h1 className="text-3xl">Order Page{cart.length}</h1>
+      <div className="col-span-4 flex flex-col justify-center items-center gap-5 p-10">
+        {
+          cart.map(product => <Review handleRemoveFromCart={handleRemoveFromCart}  key={product.id} product={product} />)
+        }
       </div>
       {/* cart container */}
       <div className="col-span-1">
---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+const Review = ({product, handleRemoveFromCart}) => {
+    const {id, img, name, price, shipping} = product;
+    return (
+        <div className='w-[571px] rounded-lg border py-1 pl-1 pr-4 flex justify-between items-center'>
+            <div className='flex gap-3 items-center'>
+                <div>
+                    <img className='w-20 rounded-md' src={img} alt="" />
+                </div>
+                <div className='py-2'>
+                    <h2 className='text-md font-semibold'>{name}</h2>
+                    <p>Price: <span className='text-yellow-400'>${price}</span></p>
+                    <p>Shipping Charge: <span className='text-yellow-400'>${shipping}</span></p>
+                </div>
+            </div>
+            <div className='bg-red-200 p-3 rounded-full hover:bg-red-100 active:border'>
+                <TrashIcon onClick={() => handleRemoveFromCart(id)} className="h-6 w-6 text-red-400" />
+            </div>
+        </div>
+    );
+};
+
+export default Review;
